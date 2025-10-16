417efd401ecb3ef8f4078b3210af1327278f3e50
PisaniValentin <valentin.pisani@hotmail.com>
2025-10-16 14:05:22 -0300

    Actualizacion middleware
    -Se corrigio el middleware para reestringir el acceso del admin a paginas como entrenador y socio

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,49 +3,12 @@ import type { NextRequest } from "next/server";
 import { auth } from "@/app/auth";
 
 export async function middleware(request: NextRequest) {
-  // const publicApiRoutes = [
-  //   { path: "/api/mercadoPago/webhook", method: "ANY" },
-  //   { path: "/api/productos", method: "GET" },
-  //   { path: "/api/productoOferta", method: "GET" },
-  //   { path: "/api/compras", method: "GET" },
-  //   { path: "/api/carrito", method: "GET" },
-  //   { path: "/api/compras", method: "POST" }, // para crear compra
-  //   { path: "/api/compras", method: "PUT" }, // para actualizar compra
-  //   { path: "/api/carrito", method: "DELETE" }, // para limpiar carrito
-  //   { path: "/api/usuario", method: "POST" }, // solo registrar
-  // ];
 
   const { pathname } = request.nextUrl;
 
-  // if (
-  //   publicApiRoutes.some(
-  //     (route) =>
-  //       pathname.startsWith(route.path) &&
-  //       (route.method === "ANY" || request.method === route.method)
-  //   )
-  // ) {
-  //   return NextResponse.next();
-  // }
 
   const session = await auth();
 
-  if (session) {
-    if (session.user?.rol === "ADMIN") {
-      if (pathname.startsWith("/entrenador")) {
-        return NextResponse.redirect(new URL("/admin", request.url));
-      }
-      return NextResponse.next();
-    }
-  }
-
-  // // ✅ Aplicar el middleware a las demás rutas API
-  // if (
-  //   pathname.startsWith("/api/") &&
-  //   !pathname.startsWith("/api/auth") &&
-  //   !session
-  // ) {
-  //   return NextResponse.redirect(new URL("/login", request.url));
-  // }
 
   // Bloquear /admin si no logueado o rol no ADMIN
   if (pathname.startsWith("/admin")) {
@@ -87,16 +50,6 @@ export async function middleware(request: NextRequest) {
       }
     }
   }
-
-  // // Bloquear /cart si no logueado o rol no CLIENTE
-  // if (pathname.startsWith("/cart")) {
-  //   if (!session) {
-  //     return NextResponse.redirect(new URL("/login", request.url));
-  //   }
-  //   if (session.user?.rol !== "ENTRENADOR") {
-  //     return NextResponse.redirect(new URL("/", request.url));
-  //   }
-  // }
 
   return NextResponse.next();
 }

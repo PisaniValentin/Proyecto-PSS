e410fe2d06e7897489581a21b6b6c36ad23479ae
SilverMigliaro <migliaro.silvestre@gmail.com>
2025-10-23 21:57:41 -0300

    Fix filtro entrenadores

--- src/app/admin/practicas/alta/page.tsx
+++ src/app/admin/practicas/alta/page.tsx
@@ -121,7 +121,7 @@ export default function AltaPracticaDeportiva() {
     }, []);
 
     const entrenadoresFiltrados = entrenadores.filter(
-        (e) => e.actividadDeportiva === deporte
+        (e: any) => e.actividadDeportiva === deporte
     );
 
     const horaFinOptions = inicio
